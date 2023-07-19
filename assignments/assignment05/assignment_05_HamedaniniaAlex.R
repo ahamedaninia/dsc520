4c5984d0718e23c3fb439c5c67bed946aaa48e26
ahamedaninia <99444626+ahamedaninia@users.noreply.github.com>
2023-07-19 13:20:25 -0700

    Update and rename assignment_05_LastnameFirstname.R to assignment_05_HamedaniniaAlex.R

--- assignments/assignment05/assignment_05_HamedaniniaAlex.R
+++ assignments/assignment05/assignment_05_HamedaniniaAlex.R
@@ -1,20 +1,20 @@
 # Assignment: ASSIGNMENT 5
-# Name: Lastname, Firstname
-# Date: 2010-02-14
+# Name: Hamedaninia, Alex
+# Date: 7/18/2023
 
 ## Set the working directory to the root of your DSC 520 directory
-setwd("/home/jdoe/Workspaces/dsc520")
+setwd("/users/alex/Documents/GitHub/dsc520")
 
 ## Load the `data/r4ds/heights.csv` to
 heights_df <- read.csv("data/r4ds/heights.csv")
 
 ## Using `cor()` compute correclation coefficients for
 ## height vs. earn
-___
+cor(heights_df$height, heights_df$earn)
 ### age vs. earn
-___
+cor(heights_df$age, heights_df$earn)
 ### ed vs. earn
-___
+cor(heights_df$ed, heights_df$earn)
 
 ## Spurious correlation
 ## The following is data on US spending on science, space, and technology in millions of today's dollars
@@ -22,4 +22,9 @@ ___
 ## Compute the correlation between these variables
 tech_spending <- c(18079, 18594, 19753, 20734, 20831, 23029, 23597, 23584, 25525, 27731, 29449)
 suicides <- c(5427, 5688, 6198, 6462, 6635, 7336, 7248, 7491, 8161, 8578, 9000)
-___
+cor(tech_spending, suicides)
+
+## We can see that the correlation is .99 or 99.2%. How incredibly high!
+## How could these two be related with such a direct correlation? 
+## Is there a third variable could be accounting for this high correlation?
+
